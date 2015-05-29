9eebaad27099a328d913e2c9a1d79efa3069d49e
dormeurduval <chaffard.joris@outlook.fr>
2015-05-29 13:23:48 +0200

    tm

--- Donnees.java
+++ Donnees.java
@@ -12,37 +12,38 @@ class Donnees {
     int selection, selectionF;
     boolean selected;
 
-    Donnees( Plateau matrice) {
+    Donnees() {
         billes = new Bille[10];
         fleches = new Fleche[20];
-        this.matrice = matrice;
+        matrice = new Plateau();
+        matrice.init_2_joueurs();
         
         depart=new Point(-1,-1);
         arrivee=new Point(-1,-1);
         
-        billes[0] = new Bille(Color.black);
-        billes[1] = new Bille(Color.black);
-        billes[2] = new Bille(Color.black);
-        billes[3] = new Bille(Color.black);
-        billes[4] = new Bille(Color.black);
+        billes[0] = new Bille(Color.black,1,0);
+        billes[1] = new Bille(Color.black,2,0);
+        billes[2] = new Bille(Color.black,0,1);
+        billes[3] = new Bille(Color.black,0,2);
+        billes[4] = new Bille(Color.black,1,1);
             
-        billes[5] = new Bille(Color.gray);
-        billes[6] = new Bille(Color.gray);
-        billes[7] = new Bille(Color.gray);
-        billes[8] = new Bille(Color.gray);
-        billes[9] = new Bille(Color.gray);
+        billes[5] = new Bille(Color.gray,4,2);
+        billes[6] = new Bille(Color.gray,2,4);
+        billes[7] = new Bille(Color.gray,3,3);
+        billes[8] = new Bille(Color.gray,4,3);
+        billes[9] = new Bille(Color.gray,3,4);
         
         for(int i = 0; i < 5; i++)
-        	fleches[i] = new Fleche(new File("/home/j/jaozande/workspace/Quits/src/t.png"));
+        	fleches[i] = new Fleche(new File("t.png"));
         
         for(int i = 5; i < 10; i++)
-        	fleches[i] = new Fleche(new File("/home/j/jaozande/workspace/Quits/src/g.png"));
+        	fleches[i] = new Fleche(new File("g.png"));
         
         for(int i = 10; i < 15; i++)
-        	fleches[i] = new Fleche(new File("/home/j/jaozande/workspace/Quits/src/bd.png"));
+        	fleches[i] = new Fleche(new File("bd.png"));
         
         for(int i = 15; i < 20; i++)
-        	fleches[i] = new Fleche(new File("/home/j/jaozande/workspace/Quits/src/bg.png"));
+        	fleches[i] = new Fleche(new File("bg.png"));
         
         
         
